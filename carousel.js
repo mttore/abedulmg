a57874018b2d716b73f81310111732e1f5646b3a
mttore <79933049+mttore@users.noreply.github.com>
2021-03-29 20:41:08 +0200

    first commit

---
+++ carousel.js
@@ -0,0 +1,11 @@
+$(".owl-carousel").owlCarousel({
+    // animateOut: 'fadeOut',
+    // autoplay: true,
+    // autoplayTimeout: 5000,
+    loop:true,
+    dots: true,
+    nav: true,
+    navText: ['<i class="fas fa-arrow-left"></i>', '<i class="fas fa-arrow-right"></i>'],
+    items: 1,
+    passive: false
+  });
